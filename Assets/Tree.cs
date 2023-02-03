43f91dad9d88cef7a991ebca9b53524b64fcab79
Dave J Rodriguez <drod7425@gmail.com>
2023-02-03 18:23:15 -0500

    initial commit

---
+++ Assets/Tree.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    
+    int levels = 5;
+
+    private void Start()
+    {
+        for(int i=0;i<levels;i++)
+        {
+            var 
+        }
+    }
+
+}
